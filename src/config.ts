e3029f336e87514b90383841a0d31b65fbf5fc7a
nadia godje <nadia.godje@edu.devinci.fr>
2024-02-26 18:12:22 +0100

    step 1 (errors)

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,16 @@
 export const REGISTRY_PORT = 8080;
 export const BASE_ONION_ROUTER_PORT = 4000;
 export const BASE_USER_PORT = 3000;
+
+import express from "express";
+import { REGISTRY_PORT } from "../config";
+
+const registry = express();
+
+registry.get("/status", (req, res) => {
+  res.send("live");
+});
+
+registry.listen(REGISTRY_PORT, () => {
+  console.log(`Registry is listening on port ${REGISTRY_PORT}`);
+});
