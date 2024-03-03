e77dcfffe7eb586ac929e52710a6cd1a5eeab79d
nadia godje <nadia.godje@edu.devinci.fr>
2024-03-03 17:24:24 +0100

    Part 1 & 2

--- src/users/user.ts
+++ src/users/user.ts
@@ -2,26 +2,31 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
 
-export type SendMessageBody = {
-  message: string;
-  destinationUserId: number;
-};
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
 
 export async function user(userId: number) {
-  const _user = express();
-  _user.use(express.json());
-  _user.use(bodyParser.json());
+    const _user = express();
+    _user.use(express.json());
+    _user.use(bodyParser.json());
 
-  // TODO implement the status route
-   _user.get("/status", (req, res) => {
-    res.send("live")
-   });
+    _user.get("/status", (req, res) => {
+        res.send("live");
+    });
 
-  const server = _user.listen(BASE_USER_PORT + userId, () => {
-    console.log(
-      `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
-    );
-  });
+    _user.get("/getLastReceivedMessage", (req, res) => {
+        res.json({ result: lastReceivedMessage });
+    });
 
-  return server;
+    _user.get("/getLastSentMessage", (req, res) => {
+        res.json({ result: lastSentMessage });
+    });
+
+    const server = _user.listen(BASE_USER_PORT + userId, () => {
+        console.log(
+            `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
+        );
+    });
+
+    return server;
 }
